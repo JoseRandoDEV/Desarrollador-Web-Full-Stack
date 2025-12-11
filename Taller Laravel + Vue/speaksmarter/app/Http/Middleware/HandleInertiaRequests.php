f6afd982ce11af1d5c94ebd6d65c4656ebc92955
JoseRandoDEV <joseandresrando@gmail.com>
2025-12-10 21:32:54 -0300

    Clase 2: Creacion de Roles y Permisos

--- Taller Laravel + Vue/speaksmarter/app/Http/Middleware/HandleInertiaRequests.php
+++ Taller Laravel + Vue/speaksmarter/app/Http/Middleware/HandleInertiaRequests.php
@@ -36,16 +36,9 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
-
-        return [
-            ...parent::share($request),
-            'name' => config('app.name'),
-            'quote' => ['message' => trim($message), 'author' => trim($author)],
-            'auth' => [
-                'user' => $request->user(),
-            ],
-            'sidebarOpen' => ! $request->hasCookie('sidebar_state') || $request->cookie('sidebar_state') === 'true',
-        ];
+        return array_merge(parent::share($request), [
+            'user.roles' => $request->user() ? $request->user()->roles->pluck('name') : [],
+            'user.permissions' => $request->user() ? $request->user()->getPermissionsViaRoles()->pluck('name') : [],
+        ]); 
     }
 }
--- Taller Laravel + Vue/speaksmarter/database/seeders/DatabaseSeeder.php
+++ Taller Laravel + Vue/speaksmarter/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->call(LevelSeeder::class);
+        $this->call([LevelSeeder::class, 
+                    RoleSeeder::class,
+                    UserSeeder::class
+                    ]);
     }
 }
